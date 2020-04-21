2c3305312cbf4072cac325a508fb0ab38839354b
Peter Weissig <peterweissig@github.com>
2020-04-21 22:48:15 +0200

    added function for checking source lists   (_config_check_sources)

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -667,6 +667,56 @@ function _config_install_list() {
     done
 }
 
+
+#***************************[check sources]***********************************
+# 2020 04 21
+
+alias _config_check_sources_vscode="_config_check_sources \
+  microsoft.com vscode"
+alias _config_check_sources_nextcloud="_config_check_sources \
+  nextcloud client"
+
+function _config_check_sources() {
+
+    # print help
+    if [ "$1" == "-h" ]; then
+        echo "$FUNCNAME <filter1> [<filter2> [<filter3>]]]"
+
+        return
+    fi
+    if [ "$1" == "--help" ]; then
+        echo "$FUNCNAME needs 1-3 parameters"
+        echo "     #.: each parameter is a pattern (e.g. a simple word)"
+        echo "          that must be found in the same line"
+        echo "          e.g. \"nextcloud\" & \"client\""
+        echo "This function checks the source lists of apt if the given"
+        echo "patterns are found in one line."
+
+        return
+    fi
+
+    # check parameter
+    if [ $# -lt 1 ] || [ $# -gt 3 ]; then
+        echo "$FUNCNAME: Parameter Error."
+        $FUNCNAME --help
+        return -1
+    fi
+
+
+    # check all source files
+    for file in /etc/apt/sources.list /etc/apt/sources.list.d/*.list; do
+        str="$(cat $file | grep -v "^#" \
+               | grep "$1" | grep "$2" | grep "$3" 2> /dev/null)"
+        if [ "$?" -ne 0 ]; then continue; fi
+        if [ "$str" != "" ]; then
+            echo "$file"
+            return
+        fi
+    done
+}
+
+
+
 #***************************[parameter]***************************************
 # 2019 12 01
 
