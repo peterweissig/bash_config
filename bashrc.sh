0c3894aacdf51890faa24a1562d4595a222be4f1
Peter Weissig <peterweissig@github.com>
2020-10-11 23:18:40 +0200

    adapted to new config file/folder policy of "repo"

--- bashrc.sh
+++ bashrc.sh
@@ -18,6 +18,7 @@ fi
 export SOURCED_BASH_CONFIG="$SOURCED_BASH_LAST"
 
 
+
 #***************************[optional external variables]*********************
 # 2019 04 21
 
@@ -27,15 +28,32 @@ if [ "$CONFIG_PATH_BACKUP" != "" ] && [ ! -d "$CONFIG_PATH_BACKUP" ]; then
     echo "path \$CONFIG_PATH_BACKUP does not exist"
 fi
 
+
+
 #***************************[paths and files]*********************************
-# 2019 04 21
+# 2020 10 11
 
 # this is only a local variable - no export
 CONFIG_PATH="$(cd "$(dirname "${BASH_SOURCE}")" && pwd )/"
 
+
 if [ "$CONFIG_PATH_BACKUP" == "" ]; then
-    export CONFIG_PATH_BACKUP="${CONFIG_PATH}backup/"
+    # check if an alternative path exists
+    if [ "$REPO_BASH_DATA_PATH" != "" ] && \
+      [ -d "$REPO_BASH_DATA_PATH" ]; then
+        export CONFIG_PATH_BACKUP="${REPO_BASH_DATA_PATH}config/"
+    else
+        export CONFIG_PATH_BACKUP="${CONFIG_PATH}backup/"
+    fi
+
+    # check if config folder exists
+    if [ ! -d "$CONFIG_PATH_BACKUP" ]; then
+        echo "creating backup folder for \"config\""
+        echo "    ($CONFIG_PATH_BACKUP)"
+        mkdir -p "$CONFIG_PATH_BACKUP"
+    fi
 fi
+
 
 
 #***************************[setting enviroment variables]********************
@@ -43,6 +61,7 @@ fi
 
 # set terminal editor to nano
 export EDITOR='nano -w'
+
 
 
 #***************************[source]******************************************
